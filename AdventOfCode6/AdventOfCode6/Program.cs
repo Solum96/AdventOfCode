812b75ea3d5a822eafb90e3709884c8bde06d349
Nils Lundell <Nils.Lundell@mardigras.se>
2020-12-06 21:13:19 +0100

    Added AdventOfCode6

---
+++ AdventOfCode6/AdventOfCode6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputData = File.ReadAllText(@"C:\Users\nilss\Source\Repos\AdventOfCode\AdventOfCode6\AdventOfCode6\TestData.txt").Split(Environment.NewLine + Environment.NewLine);
+            var inputArr = inputData.Select(i => i.Split(Environment.NewLine)).ToArray();
+
+            var partOne = inputArr
+                            .Select(group => group
+                                .SelectMany(i => i)
+                                .Distinct()
+                                .Count()
+                            ).Sum();
+
+            var partTwo = inputArr
+                            .Select(group => group
+                                .SelectMany(i => i)
+                                .Distinct()
+                                .Where(x => group
+                                    .All(y => y
+                                    .Contains(x)))
+                                .Count()
+                            ).Sum();
+
+            Console.WriteLine(partOne);
+            Console.WriteLine(partTwo);
+        }
+    }
+}
