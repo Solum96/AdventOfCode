647c49802c2778df0e5c9a10b587e0bfa6faf2d7
Nils Lundell <Nils.Lundell@mardigras.se>
2020-12-06 21:26:48 +0100

    Deleted it all and rewrote it to learn

--- AdventOfCode6/AdventOfCode6/Program.cs
+++ AdventOfCode6/AdventOfCode6/Program.cs
@@ -10,24 +10,24 @@ namespace AdventOfCode6
         static void Main(string[] args)
         {
             var inputData = File.ReadAllText(@"C:\Users\nilss\Source\Repos\AdventOfCode\AdventOfCode6\AdventOfCode6\TestData.txt").Split(Environment.NewLine + Environment.NewLine);
-            var inputArr = inputData.Select(i => i.Split(Environment.NewLine)).ToArray();
+            var inputArr = inputData.Select(i => i.Split(Environment.NewLine));
 
             var partOne = inputArr
-                            .Select(group => group
-                                .SelectMany(i => i)
-                                .Distinct()
-                                .Count()
-                            ).Sum();
+                .Select(group => group
+                    .SelectMany(a => a)
+                    .Distinct()
+                    .Count())
+                .Sum();
 
             var partTwo = inputArr
-                            .Select(group => group
-                                .SelectMany(i => i)
-                                .Distinct()
-                                .Where(x => group
-                                    .All(y => y
-                                    .Contains(x)))
-                                .Count()
-                            ).Sum();
+                .Select(group => group
+                    .SelectMany(a => a)
+                    .Distinct()
+                    .Where(b => group
+                        .All(c => c
+                            .Contains(b)))
+                    .Count())
+                .Sum();
 
             Console.WriteLine(partOne);
             Console.WriteLine(partTwo);
